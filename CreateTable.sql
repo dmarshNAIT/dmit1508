c83bdeefccff52db16a879040973775757183426
Dana <55507008+dmarshNAIT@users.noreply.github.com>
2021-02-02 11:53:43 -0700

    feb 2 class examples

--- CreateTable.sql
+++ CreateTable.sql
@@ -1,7 +1,10 @@
+-- pick the database to use for this script:
+USE sandbox
+
 -- remove any previous versions of tables:
-DROP TABLE Project
-DROP TABLE EmployeeProject
-DROP TABLE Employee
+DROP TABLE IF EXISTS Project
+DROP TABLE IF EXISTS EmployeeProject
+DROP TABLE IF EXISTS Employee
 
 -- CREATE statements:
 CREATE TABLE Employee (
@@ -10,19 +13,29 @@ CREATE TABLE Employee (
 ,	FirstName	VARCHAR(30) NOT NULL
 )
 
-CREATE TABLE EmployeeProject (
-	EmployeeID		CHAR(11)	NOT NULL
-,	ProjectNumber	INT			NOT NULL
-,	WeeklyHours		INT			NOT NULL
-	-- because this constraint affects MORE than one column, it is listed as a table-level constraint:
-,	CONSTRAINT PK_EmployeeProject PRIMARY KEY CLUSTERED (EmployeeID, ProjectNumber)
-)
-
 CREATE TABLE Project (
 	ProjectNumber	INT IDENTITY(1,1)	NOT NULL CONSTRAINT PK_Project PRIMARY KEY CLUSTERED
 ,	ProjectName		VARCHAR(50)			NOT NULL
 ,	ProjectLocation VARCHAR(50)			NOT NULL
 )
+
+CREATE TABLE EmployeeProject (
+	EmployeeID		CHAR(11)	NOT NULL 
+		CONSTRAINT FK_EmployeeProjectToEmployee REFERENCES Employee (EmployeeID)
+,	ProjectNumber	INT			NOT NULL
+		CONSTRAINT FK_EmployeProjectToProject REFERENCES Project (ProjectNumber)
+,	WeeklyHours		INT			NOT NULL
+		CONSTRAINT CK_WeeklyHoursMax CHECK (WeeklyHours <= 20)
+	-- because this constraint affects MORE than one column, it is listed as a table-level constraint:
+,	CONSTRAINT PK_EmployeeProject PRIMARY KEY CLUSTERED (EmployeeID, ProjectNumber)
+)
+
+CREATE TABLE Department (
+    DepartmentNumber	INT	IDENTITY (1,1)	NOT NULL CONSTRAINT PK_Department PRIMARY KEY CLUSTERED
+,	DepartmentName		VARCHAR(30)			NOT NULL
+)
+
+
 
 -- verify:
 EXEC sp_help Employee
