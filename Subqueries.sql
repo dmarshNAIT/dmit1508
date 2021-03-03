ceb8e4679010aaf2dccff70148622bc822be7ef4
Dana <55507008+dmarshNAIT@users.noreply.github.com>
2021-03-03 12:10:30 -0700

    Mar 3 class examples

--- Subqueries.sql
+++ Subqueries.sql
@@ -22,17 +22,46 @@ WHERE PaymentTypeDescription = 'Cash'
 
 
 --2. Select The Student IDs of all the students that are in the 'Association of Computing Machinery' club
+SELECT StudentID
+FROM Activity
+WHERE ClubId IN (SELECT ClubId FROM Club WHERE ClubName = 'Association of Computing Machinery')
 
+SELECT StudentID
+FROM Activity
+INNER JOIN Club ON Activity.ClubId = Club.ClubId
+WHERE ClubName = 'Association of Computing Machinery'
 
 --3. Select All the staff full names that have taught a course.
+SELECT FirstName + ' ' + LastName AS FullName
+FROM Staff
+WHERE StaffID IN (SELECT StaffID FROM Offering)
 
+SELECT DISTINCT FirstName + ' ' + LastName AS FullName
+FROM Staff
+INNER JOIN Offering ON Staff.StaffID = Offering.StaffID
 
 --4. Select All the staff full names that taught DMIT172.
+SELECT FirstName + ' ' + LastName AS FullName
+FROM Staff
+WHERE StaffID IN (SELECT StaffID FROM Offering WHERE CourseID = 'DMIT172')
 
+SELECT DISTINCT FirstName + ' ' + LastName AS FullName
+FROM Staff
+INNER JOIN Offering ON Staff.StaffID = Offering.StaffID
+WHERE CourseID = 'DMIT172'
 
 --5. Select All the staff full names that have never taught a course
 -- outer query: Select staff names from Staff WHERE StaffID is NOT in the inner query
 -- inner query: getting a list of staff IDs from Offering table
+
+SELECT FirstName + ' ' + LastName AS FullName
+FROM Staff
+WHERE StaffID NOT IN (SELECT StaffID FROM Offering)
+
+SELECT DISTINCT FirstName + ' ' + LastName AS FullName
+FROM Staff
+LEFT JOIN Offering ON Staff.StaffID = Offering.StaffID
+WHERE Offering.StaffID IS NULL
 
 --6. Select the Payment TypeID(s) that have the highest number of Payments made.
 SELECT PaymentTypeID
@@ -45,10 +74,27 @@ HAVING COUNT(*) >= ALL (
 )
 
 --7. Select the Payment Type Description(s) that have the highest number of Payments made.
--- DO THIS ONE
+SELECT PaymentTypeDescription
+FROM Payment
+INNER JOIN PaymentType ON Payment.PaymentTypeID = PaymentType.PaymentTypeID
+GROUP BY PaymentType.PaymentTypeID, PaymentTypeDescription
+HAVING COUNT(*) >= ALL (
+		SELECT COUNT(*)
+		FROM Payment
+		GROUP BY PaymentTypeID
+)
 
 --8. What is the total avg mark for the students from Edmonton?
+SELECT AVG(Mark) AS AverageMark
+FROM Registration
+WHERE StudentID IN (
+	SELECT StudentID FROM Student WHERE City = 'Edmonton'
+)
 
+SELECT AVG(Mark) AS AverageMark
+FROM Registration
+INNER JOIN Student on Registration.StudentID = Student.StudentID
+WHERE City = 'Edmonton'
 
 --9. What is the avg mark for each of the students from Edmonton? Display their StudentID and avg(mark)
 SELECT StudentID, AVG(Mark) AS AverageMark
