a27c02ee8bd6c3eb66f34d0792920efa0b704d45
Dana <55507008+dmarshNAIT@users.noreply.github.com>
2021-03-16 11:45:22 -0600

    Mar 16 class examples

--- FlowControlAndVariablesExercise.sql
+++ FlowControlAndVariablesExercise.sql
@@ -2,7 +2,6 @@
 USE IQSchool
 GO
 
-
 --1.	Create a stored procedure called StudentClubCount. 
 -- It will accept a clubID as a parameter.
 -- If the count of students in that club is greater than 2 print ‘A successful club!’ . If the count is not greater than 2 print ‘Needs more members!’.
@@ -36,6 +35,12 @@ RETURN -- end of the procedure
 
 -- test: 'ACM' should be successful
 -- test: 'CHESS' will need more members
+
+
+
+
+
+
 
 --2.	Create a stored procedure called BalanceOrNoBalance. It will accept a studentID as a parameter. 
 -- Each course has a cost. If the total of the costs for the courses the student is registered in is more than the total of the payments that student has made then print ‘balance owing !’ otherwise print ‘Paid in full! Welcome to School 158!’
@@ -75,6 +80,9 @@ ELSE
 	PRINT 'Paid in full. Welcome!'
 	END
 
+
+
+
 --3.	Create a stored procedure called ‘DoubleOrNothin’. It will accept a students first name and last name as parameters. 
 -- If the student name already is in the table then print ‘We already have a student with the name firstname lastname!’ Other wise print ‘Welcome firstname lastname!’
 
@@ -102,19 +110,44 @@ ELSE
 -- test with: 'Winnie Woo' already exists
 -- test with your own name
 
+
+
+
+
 --4.	Create a procedure called ‘StaffRewards’. It will accept a staff ID as a parameter. 
 -- If the number of classes the staff member has ever taught is between 0 and 2 print ‘Well done!’, if it is between 3 and 5 print ‘Exceptional effort!’, if the number is greater than 5 print ‘Totally Awesome Dude!’
 
 -- create a variable to hold the StaffID we're interested in
+DECLARE @StaffID INT
+SET @StaffID = 4 -- test data
 
 -- create a variable called @NumberClassesTaught
+DECLARE @NumberClassesTaught INT
 
 -- asssign it a value:
+SELECT @NumberClassesTaught = COUNT(*)
+FROM Offering
+WHERE StaffID = @StaffID
 
 -- IF @NumberClassesTaught <=2: print 'Well done!'
+IF @NumberClassesTaught BETWEEN 0 AND 2
+	BEGIN
+	PRINT 'Well done!'
+	END
+ELSE 
+	BEGIN
 -- ELSE
 	-- IF @NumberClassesTaught <=5: print 'Exceptional effort!'
+	IF @NumberClassesTaught BETWEEN 3 AND 5
+		BEGIN
+		PRINT 'Exceptional effort!'
+		END
 	-- ELSE: PRINT 'Totally awesome, dude!"
+	ELSE
+		BEGIN
+		PRINT 'Totally awesome, dude!'
+		END -- ends the inner else
+	END -- ends the outer else
 
 -- test: staffID 4 should be 'totally awesome'
 -- staffID 6 should be 'exceptional'
