5a4af1a5541963a726387aeeb1651fdbb40e784a
Dana <55507008+dmarshNAIT@users.noreply.github.com>
2021-07-29 21:15:10 -0600

    Jul 29 class examples

--- Triggers-Sandbox.sql
+++ Triggers-Sandbox.sql
@@ -88,3 +88,87 @@ UPDATE MovieCharacter SET Characterwage = -50 WHERE CharacterID > 4
 
 -- how to check which fields are IDENTITY:
 exec sp_help MovieCharacter
+GO
+
+-- practice Q #3:
+-- Create a trigger that enforces a rule that an AgentFee cannot be increased by more than 100% in one update.
+
+CREATE TRIGGER TR_PracticeQ3
+	ON Agent
+	FOR UPDATE
+AS
+
+IF @@ROWCOUNT > 0 AND UPDATE(AgentFee)
+	BEGIN
+
+	-- look at the old value for AgentFee: deleted.AgentFee
+	-- compare to the NEW value for Agent Fee: inserted.AgentFee
+	-- if new value > 2 * old value: RAISERROR & ROLLBACK
+	IF EXISTS (
+		SELECT *
+		FROM inserted
+		INNER JOIN deleted ON inserted.AgentID = deleted.AgentID
+		WHERE inserted.AgentFee > 2* deleted.AgentFee
+	) -- we've broken the rule :(
+		BEGIN
+		ROLLBACK TRANSACTION
+		RAISERROR('Too much of a change', 16, 1)
+		END
+	END
+RETURN
+GO
+
+-- test:
+-- UPDATE zero records
+UPDATE Agent SET AgentFee = 100 WHERE AgentFee = 500 -- no agent exists
+-- UPDATE 1 record successfully
+UPDATE Agent SET AgentFee = 75 WHERE AgentID = 1
+SELECT * FROM Agent
+-- UPDATE 1 record unsuccessfully
+UPDATE Agent SET AgentFee = 750 WHERE AgentID = 1
+SELECT * FROM Agent
+-- UPDATE many records
+UPDATE Agent SET AgentFee = 100
+SELECT * FROM Agent
+
+GO
+-- practice Q #4
+-- Create a trigger that enforces a rule that a MovieCharacter cannot be deleted if their Agent's AgentFee is >= 50.
+
+CREATE TRIGGER TR_PracticeQ4
+	ON MovieCharacter
+	FOR DELETE
+AS
+
+-- make sure at least 1 character is being deleted
+IF @@ROWCOUNT > 0
+	BEGIN
+	-- check if any deleted records have an agent whose fee is >=50]
+	IF EXISTS (
+		SELECT * 
+		FROM deleted
+		INNER JOIN Agent ON deleted.AgentID = Agent.AgentID
+		WHERE AgentFee >=50
+	)
+		BEGIN
+		RAISERROR('cannot delete', 16, 1)
+		ROLLBACK TRANSACTION
+		END
+	END
+RETURN
+GO
+
+-- test
+SELECT * FROM MovieCharacter
+SELECT * FROM Agent
+
+-- delete ZERO records
+DELETE FROM MovieCharacter WHERE CharacterID = 99999
+SELECT * FROM MovieCharacter
+-- delete 1 record successfully
+DELETE FROM MovieCharacter WHERE CharacterID = 1
+SELECT * FROM MovieCharacter
+-- delete many records (including 1 with a high-paid agent): should fail
+DELETE FROM MovieCharacter WHERE AgentID = 2
+SELECT * FROM MovieCharacter
+
