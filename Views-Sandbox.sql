144ea91a0f2bf231c9b3a4e3b1850f7f69072354
Dana <55507008+dmarshNAIT@users.noreply.github.com>
2022-03-04 21:00:49 -0700

    Mar 4 class examples

---
+++ Views-Sandbox.sql
@@ -0,0 +1,31 @@
+-- creating a view:
+CREATE VIEW StudentView
+AS
+SELECT FirstName + ' ' + LastName AS FullName
+	, CourseName
+	, Mark
+FROM Student
+INNER JOIN Registration ON Student.StudentID = Registration.StudentID
+INNER JOIN Offering ON Registration.OfferingCode = Offering.OfferingCode
+INNER JOIN Course ON Offering.CourseID = Course.CourseId
+
+-- now, we can select from the view just like we select from a table:
+SELECT * FROM StudentView
+
+-- if we need to modify the view, we just change the word CREATE to ALTER, make adjustments, then re-run:
+ALTER VIEW StudentView
+AS
+SELECT FirstName + ' ' + LastName AS FullName
+	, Course.CourseID -- this is new!
+	, CourseName
+	, Mark
+FROM Student
+INNER JOIN Registration ON Student.StudentID = Registration.StudentID
+INNER JOIN Offering ON Registration.OfferingCode = Offering.OfferingCode
+INNER JOIN Course ON Offering.CourseID = Course.CourseId
+
+-- to see the definition of an existing view:
+exec sp_helptext StudentView
+
+-- and to delete a view:
+DROP VIEW StudentView
