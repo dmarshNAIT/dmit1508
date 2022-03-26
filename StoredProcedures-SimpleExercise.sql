481d3bc00483d7bb094a6cd84d2a8b7b4264bfec
Dana <55507008+dmarshNAIT@users.noreply.github.com>
2022-03-25 19:37:54 -0600

    Mar 25 class examples

--- StoredProcedures-SimpleExercise.sql
+++ StoredProcedures-SimpleExercise.sql
@@ -6,18 +6,84 @@ GO
 --1. Create a stored procedure called “HonorCourses” to select all the course names that have averages >80%. 
 
 --2. Create a stored procedure called “HonorCoursesOneTerm” to select all the course names that have average >80% in semester A100.
+CREATE PROCEDURE HonorCoursesOneTerm AS
+
+SELECT CourseName
+FROM Course
+INNER JOIN Offering ON Course.CourseId = Offering.CourseID
+INNER JOIN Registration ON Offering.OfferingCode = Registration.OfferingCode
+WHERE SemesterCode = 'A100'
+GROUP BY Course.CourseId, CourseName
+HAVING AVG(Mark) > 80
+
+RETURN
+GO
 
 --3. Oops, made a mistake! For question 2, it should have been for semester A200. Write the code to change the procedure accordingly. 
+ALTER PROCEDURE HonorCoursesOneTerm AS
+
+SELECT CourseName
+FROM Course
+INNER JOIN Offering ON Course.CourseId = Offering.CourseID
+INNER JOIN Registration ON Offering.OfferingCode = Registration.OfferingCode
+WHERE SemesterCode = 'A200'
+GROUP BY Course.CourseId, CourseName
+HAVING AVG(Mark) > 80
+
+RETURN
+GO
 
 --4. Create a stored procedure called “NotInDMIT221” that lists the full names of the staff that have not taught DMIT221.
 
+-- select staff names
+-- where staff name is NOT in a list o staff who HAVE taught DMIT221
+CREATE PROCEDURE NotInDMIT221 AS
+
+SELECT FirstName + ' ' + LastName AS FullName
+FROM Staff
+WHERE StaffID NOT IN (
+	SELECT StaffID FROM Offering WHERE CourseID = 'DMIT221'
+)
+
+RETURN
+
 --5. Create a stored procedure called “LowNumbers” to select the course name of the course(s) that have had the lowest number of students in it. Assume all courses have registrations.
+
+CREATE PROCEDURE LowNumbers AS
+
+SELECT CourseName
+	--, COUNT(StudentID) AS NumberOfStudents
+FROM Course
+INNER JOIN Offering ON Course.CourseID = Offering.CourseID
+INNER JOIN Registration ON Offering.OfferingCode = Registration.OfferingCode
+GROUP BY Course.CourseID, CourseName
+HAVING COUNT(StudentID) <= ALL (
+	SELECT COUNT(StudentID)
+	FROM Registration
+	INNER JOIN Offering ON Offering.OfferingCode = Registration.OfferingCode
+	GROUP BY CourseID
+)
+
+RETURN -- end of SP
+GO -- end of a batch
+
+EXEC LowNumbers
+GO
 
 --6. Create a stored procedure called “Provinces” to list all the students provinces.
 
 --7. OK, question 6 was ridiculously simple and serves no purpose. Lets remove that stored procedure from the database.
 
 --8. Create a stored procedure called StudentPaymentTypes that lists all the student names and their payment type Description. Ensure all the student names are listed.
+
+CREATE PROCEDURE StudentPaymenTypes AS
+
+SELECT FirstName, LastName, PaymentTypeDescription
+FROM Student
+LEFT JOIN Payment ON Student.StudentID = Payment.StudentID
+LEFT JOIN PaymentType ON Payment.PaymentTypeID = PaymentType.PaymentTypeID
+
+RETURN
 
 --9. Modify the procedure from question 8 to return only the student names that have made payments.
 
