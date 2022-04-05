1a595301d97a275b12505e4c59c5117b7f74d9e8
Dana <55507008+dmarshNAIT@users.noreply.github.com>
2022-04-04 20:50:53 -0600

    Aor 4 class examples

--- Triggers-Sandbox.sql
+++ Triggers-Sandbox.sql
@@ -29,12 +29,13 @@ SELECT * FROM Student -- base table
 PRINT 'Aaaand we are done.'
 
 RETURN -- end of the trigger
+GO
 
 -- test:
 -- UPDATE 0 rows:
 UPDATE Student
 SET BalanceOwing = 999
-WHERE StudentID = 4359853498543
+WHERE StudentID = 456456456
 
 -- UPDATE 1 row:
 UPDATE Student
@@ -42,3 +43,37 @@ SET BalanceOwing = 100
 WHERE StudentID = 198933540
 
 -- UPDATE many rows:
+UPDATE Student
+SET BalanceOwing = 500
+WHERE City = 'Edmonton'
+
+GO
+
+--- practice Q #2:
+-- Create a trigger to enforce a rule that CharacterWages must be >= 0.
+
+USE MovieCharacter
+GO
+
+CREATE TRIGGER TR_PracticeQ_1
+	ON MovieCharacter
+	FOR INSERT, UPDATE -- triggered by BOTH insert & update operations on the MovieCharacter table
+AS
+
+IF @@ROWCOUNT > 0 AND UPDATE(CharacterWage)
+	-- @@rowcount tells us how many rows were affected by the last DML statement.
+	-- if no rows were affected, our trigger has nothing to do. 
+	-- UPDATE() tells us if a specific column was updated by the last DML statement. It returns TRUE if it was, FALSE otherwise.
+	BEGIN
+
+	IF EXISTS (SELECT * FROM inserted WHERE CharacterWage < 0) -- check if the rule was violated
+		BEGIN -- if so, ROLLBACK
+		RAISERROR('Cannot have negative wage, you monster', 16, 1)
+		ROLLBACK TRANSACTION 
+		END
+	END
+
+RETURN
+GO
+
+-- test on Wednesday
