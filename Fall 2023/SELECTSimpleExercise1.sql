17ddc0a2e58e63d07f9ff6abd24b7ab2794790ff
Dana <dmarsh@nait.ca>
2023-10-12 13:52:15 -0600

    Complete sample solution

--- Fall 2023/SELECTSimpleExercise1.sql
+++ Fall 2023/SELECTSimpleExercise1.sql
@@ -4,16 +4,32 @@ USE IQSchool
 GO
 
 --1.	Select all the information from the club table
+SELECT ClubID, ClubName
+FROM Club
 
 --2.	Select the FirstNames and LastNames of all the students
+SELECT FirstName, LastName
+FROM Student
 
 --3.	Select all the CourseId and CourseName of all the coureses. Use the column aliases of Course ID and Course Name
+SELECT CourseId AS 'Course ID'
+,	CourseName AS 'Course Name'
+FROM Course
 
 --4.	Select all the course information for courseID 'DMIT1001'
+SELECT CourseId, CourseName, CourseHours, MaxStudents, CourseCost
+FROM Course
+WHERE CourseId = 'DMIT1001'
 
 --5.	Select the Staff names who have positionID of 3
+SELECT FirstName + ' ' + LastName AS StaffName
+FROM Staff
+WHERE PositionID = 3
 
 --6.	select the CourseNames whose CourseHours are less than 96
+SELECT CourseName
+FROM Course
+WHERE CourseHours < 96
 
 --7.	Select the studentIDs, OfferingCode and mark where the Mark is between 70 and 80
 SELECT StudentID, OfferingCode, Mark
@@ -24,11 +40,21 @@ SELECT StudentID, OfferingCode, Mark
 FROM Registration
 WHERE Mark >= 70 AND Mark <= 80
 
---8.	Select the studentID's, Offering Code and mark where the Mark is between 70 and 80 and the OfferingCode is 1001 or 1009
+--8.	Select the studentIDs, Offering Code and mark where the Mark is between 70 and 80 and the OfferingCode is 1001 or 1009
+SELECT StudentID, OfferingCode, Mark
+FROM Registration
+WHERE Mark BETWEEN 70 AND 80
+	AND (OfferingCode = 1001 OR OfferingCode = 1009)
 
 --9.	Select the students first and last names who have last names starting with S
+SELECT FirstName, LastName
+FROM Student
+WHERE LastName LIKE 'S%'
 
 --10.	Select Coursenames whose CourseID  have a 1 as the fifth character
+SELECT CourseName
+FROM Course
+WHERE CourseId LIKE '____1%'
 
 --11.	Select the CourseIDs and Coursenames where the CourseName contains the word 'programming'
 SELECT CourseID, CourseName
@@ -36,6 +62,13 @@ FROM Course
 WHERE CourseName LIKE '%programming%'
 
 --12.	Select all the ClubNames who start with N or C.
+SELECT ClubId, ClubName
+FROM Club
+WHERE ClubName LIKE 'N%' OR ClubName LIKE 'C%'
+
+SELECT ClubId, ClubName
+FROM Club
+WHERE ClubName LIKE '[NC]%'
 
 --13.	Select Student Names, Street Address and City where the LastName is only 3 letters long.
 SELECT FirstName + ' ' + LastName AS StudentName
@@ -44,5 +77,8 @@ SELECT FirstName + ' ' + LastName AS StudentName
 FROM Student
 WHERE LastName LIKE '___'
 
---14.	Select all the StudentID's where the PaymentAmount < 500 OR the PaymentTypeID is 5
+--14.	Select all the StudentIDs where the PaymentAmount < 500 OR the PaymentTypeID is 5
+SELECT StudentID
+FROM Payment
+WHERE Amount < 500 OR PaymentTypeID = 5
 
