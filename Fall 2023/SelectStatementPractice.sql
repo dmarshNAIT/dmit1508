2dc8c96c4c8594bc6d267113c28b35e5fc53d531
Dana <dmarsh@nait.ca>
2023-10-12 14:49:19 -0600

    Oct 12 class examples

--- Fall 2023/SelectStatementPractice.sql
+++ Fall 2023/SelectStatementPractice.sql
@@ -18,3 +18,54 @@ WHERE BalanceOwing > 0
 SELECT FirstName + ' '  + LastName AS FullName
 FROM Student
 WHERE FirstName LIKE '%a%'
+
+-- Oct 12: More WHERE practice
+SELECT FirstName, LastName, City
+FROM Student
+WHERE City = 'Edmonton' OR City = 'Leduc'
+-- this is the same as:
+SELECT FirstName, LastName, City
+FROM Student
+WHERE City IN ('Edmonton', 'Leduc')
+
+-- now let's do the opposite
+SELECT FirstName, LastName, City
+FROM Student
+WHERE City != 'Edmonton' AND City != 'Leduc'
+-- or
+SELECT FirstName, LastName, City
+FROM Student
+WHERE City NOT IN ('Edmonton', 'Leduc')
+
+-- Aggregations
+-- the overall average of all the marks in the Registration table
+SELECT AVG(Mark) AS AverageMark
+FROM Registration
+-- what if we want the average for a particular student?
+SELECT AVG(Mark) AS AverageMark
+FROM Registration
+WHERE StudentID = 200122100
+
+-- what is the total amount of all payments received?
+SELECT SUM(Amount) AS TotalPayments
+	, MIN(Amount) AS SmallestPayment
+	, MAX(Amount) AS LargestPayment
+FROM Payment
+
+-- COUNT(*) counts the # of records
+-- COUNT(ColumnName) counts the # of non-null values
+SELECT COUNT(*) AS NumberOfRows
+	, COUNT(DateReleased) AS NumberOfExStaff
+FROM Staff
+
+-- using GROUP BY
+SELECT	StudentID 
+,	AVG(Mark) AS AverageMark
+FROM Registration
+GROUP BY StudentID -- average mark PER student
+
+SELECT	StudentID 
+,	AVG(Mark) AS AverageMark
+FROM Registration
+GROUP BY StudentID
+HAVING Avg(Mark) > 80
