24c3d39c3015340955f4c5f9eecd339b939177fc
Dana <dmarsh@nait.ca>
2023-10-27 14:55:32 -0600

    Posted sample solutions

--- Fall 2023/DML_UpdateExercise.sql
+++ Fall 2023/DML_UpdateExercise.sql
@@ -1,20 +1,41 @@
 --Update Exercise
-
 --Use the Memories Forever database for this exercise.The script to create the tables is on Moodle if you have not competed the create tables exercise. Data is inserted into the tables when completing the Insert Exercise.
+USE MemoriesForever
+GO
 --If an update fails write a brief explanation why. Do not just quote the error message genereated by the server!
 
 --1. Update the following records in the ItemType Table:
---OLD RECORD	NEW RECORD
---ItemTypeID	ItemTypeDescription	ItemTypeID	ItemTypeDescription
---1	Camera	5	Camera
---2	Lights	2	Bright Lights
+UPDATE ItemType
+SET ItemTypeID = 5
+WHERE ItemTypeID = 1
+--Fails: Changing the Primary key value causes a related recorded in Item to lose its parent record in ItemType. 
+--Updating the primary key value is not recomended!!!!
 
+UPDATE ItemType
+SET ItemTypeDescription = 'Bright Lights'
+WHERE ItemTypeID = 2
+GO
 
 --2. Update the following records in the Item Table:
 
 --•	ItemID 1 is now $30/day and called a Canon G3
 --•	ItemID 4 is now ItemTypeID 5
 --•	ItemID 4 is now $30/day
+
+UPDATE Item
+SET PricePerDay = 30, ItemDescription = 'Canon G3'
+WHERE ItemID = 1
+
+UPDATE Item
+SET ItemTypeID = 5
+WHERE ItemID = 4
+--Fails: There is no ItemTypeId 5. Foreign Key values must have a matching value in the parent table.
+
+UPDATE Item
+SET PricePerDay = 30
+WHERE ItemID = 4
+GO
+
 
 --3. Update the following records in the Staff Table:
 
@@ -24,3 +45,15 @@
 --Wage: $23.00
 --•	Update StaffID 12 to have a wage of 80 (note the message displayed)
 
+UPDATE Staff
+SET Wage = 19
+WHERE StaffID = 1
+
+UPDATE Staff
+SET StaffLastName = 'Pic', Wage = 23
+WHERE StaffID = 2
+
+UPDATE Staff
+SET Wage = 80
+WHERE StaffID = 12
+-- 0 rows affected: because there is no StaffID 12. No records matched our filter.
--- Fall 2023/SELECT_Union.sql
+++ Fall 2023/SELECT_Union.sql
@@ -1,5 +1,7 @@
 --Union Exercise
 --(use the IQSchool database)
+USE IQSchool
+GO
 
 --1.	Write the code that will produce the ‘It Happened in October’ output.
 --The output is shown below:
@@ -11,14 +13,17 @@
 --7           Staff Hired:Hugh Guy
 --6           Staff Hired:Sia Latter
 
---Additional Info:
 
----	if the event is a staff being hired:
----	the id column contains the employee id
----	the name is in the format ‘FirstName LastName’
----	if the event is a student birthdate:
----	the id column contains the student id
----	the name is in the format ‘FirstName LastName’
----	the data is sorted in descending order of id (Student or Staff)
----	the display is limited to the hiring of staff or the birthdates of students in the month of October
+SELECT StaffID AS ID												---	the id column contains the employee id
+	, 'Staff Hired:' + FirstName + ' ' + LastName AS [Event:Name]	---	the name is in the format ‘FirstName LastName’
+FROM Staff
+WHERE DATENAME(mm, DateHired) = 'October'							--- hiring of staff in October
 
+UNION ALL
+
+SELECT StudentID													---	the id column contains the student id	
+,	'Student Born:' + FirstName + ' ' + LastName					---	the name is in the format ‘FirstName LastName’
+FROM Student
+WHERE DATENAME(mm, Birthdate) = 'October'							---	birthdates of students in the month of October
+
+ORDER BY ID DESC													---	sorted in descending order of id
