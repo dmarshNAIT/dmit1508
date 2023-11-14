f32ec24d13ff73010596fcdc5611976f87121478
Dana <dmarsh@nait.ca>
2023-11-14 10:58:04 -0700

    Nov 14 class examples

--- Fall 2023/StoredProcedures_FlowControlAndVariables.sql
+++ Fall 2023/StoredProcedures_FlowControlAndVariables.sql
@@ -4,6 +4,8 @@ USE IQSchool
 GO
 
 --1.	Create a stored procedure called StudentClubCount. It will accept a clubID as a parameter. If the count of students in that club is greater than 2 print ‘A successful club!’. If the count is not greater than 2 print ‘Needs more members!’.
+DROP PROCEDURE IF EXISTS StudentClubCount
+GO
 
 CREATE PROCEDURE StudentClubCount ( @ClubID VARCHAR(10) = NULL )
 AS
@@ -47,93 +49,125 @@ EXEC StudentClubCount 'I hate Databases' -- this club doesn't exist; a bonus cha
 --2.	Create a stored procedure called BalanceOrNoBalance. It will accept a studentID as a parameter. Each course has a cost. If the total of the costs for the courses the student is registered in is more than the total of the payments that student has made, then print ‘balance owing!’ otherwise print ‘Paid in full! Welcome to IQ School!’
 --Do Not use the BalanceOwing field in your solution. 
 
--- create a variable called @StudentID and assign it any literal value
-DECLARE @StudentID INT 
-SET @StudentID = 199899200
+DROP PROCEDURE IF EXISTS BalanceOrNoBalance
+GO
 
--- create a variable called @TotalCourseCost
-DECLARE @TotalCourseCost DECIMAL(8,2)
 
--- create a variable called @TotalPayments
-DECLARE @TotalPayments DECIMAL(8,2)
+CREATE PROCEDURE BalanceOrNoBalance(@StudentID INT = NULL)
+AS
 
--- assign each variable a value using data from our DB
-SELECT @TotalCourseCost = SUM(CourseCost)
-FROM Registration
-INNER JOIN Offering ON Registration.OfferingCode = Offering.OfferingCode
-INNER JOIN Course ON Course.CourseId = Offering.CourseId
-WHERE StudentID = @StudentID
-
-SELECT @TotalPayments = SUM(Amount) 
-FROM Payment 
-WHERE StudentID = @StudentID
-
--- If the total of the costs for the courses the student is registered in is more than the total of the payments that student has made, then print ‘balance owing!’ otherwise print ‘Paid in full! Welcome to IQ School!’
-IF @TotalCourseCost > @TotalPayments
+IF @StudentID IS NULL
 	BEGIN
-	PRINT 'Balance Owing'
+	RAISERROR('StudentID is a required parameter.', 16, 1)
 	END
-ELSE
+ELSE -- start of outer ELSE
 	BEGIN
-	PRINT 'Paid in full: Welcome to IQ School!'
-	END
+	-- create a variable called @TotalCourseCost
+	DECLARE @TotalCourseCost DECIMAL(8,2)
 
+	-- create a variable called @TotalPayments
+	DECLARE @TotalPayments DECIMAL(8,2)
 
+	-- assign each variable a value using data from our DB
+	SELECT @TotalCourseCost = SUM(CourseCost)
+	FROM Registration
+	INNER JOIN Offering ON Registration.OfferingCode = Offering.OfferingCode
+	INNER JOIN Course ON Course.CourseId = Offering.CourseId
+	WHERE StudentID = @StudentID
+
+	SELECT @TotalPayments = SUM(Amount) 
+	FROM Payment 
+	WHERE StudentID = @StudentID
+
+	-- If the total of the costs for the courses the student is registered in is more than the total of the payments that student has made, then print ‘balance owing!’ otherwise print ‘Paid in full! Welcome to IQ School!’
+	IF @TotalCourseCost > @TotalPayments
+		BEGIN
+		PRINT 'Balance Owing'
+		END
+	ELSE
+		BEGIN
+		PRINT 'Paid in full: Welcome to IQ School!'
+		END
+	END -- this ends the outer ELSE block
+
+RETURN
+GO
 
 
 --3.	Create a stored procedure called ‘DoubleOrNothin’. It will accept a student’s first name and last name as parameters. If the student’s name already is in the table, then print ‘We already have a student with the name firstname lastname!’ Otherwise print ‘Welcome firstname lastname!’
 
--- create variables called @FirstName and @LastName
-DECLARE @FirstName VARCHAR(25)
-DECLARE @LastName VARCHAR(25)
+DROP PROCEDURE IF EXISTS DoubleOrNothin
+GO
 
--- assign each variable a literal value
-SET @FirstName = 'Winnie'
-SET @LastName = 'Woot'
+CREATE PROCEDURE DoubleOrNothin (
+	@FirstName VARCHAR(25) = NULL
+,	@LastName VARCHAR(25) = NULL
+)
+AS
+
+IF @FirstName IS NULL OR @LastName IS NULL
+	BEGIN
+	RAISERROR('First and last names are required params', 16, 1)
+	END
 
 -- If the student’s name already is in the table, then print ‘We already have a student with the name firstname lastname!’ Otherwise print ‘Welcome firstname lastname!’
-IF EXISTS(SELECT * FROM Student WHERE FirstName = @FirstName AND LastName = @LastName)
+ELSE IF EXISTS(SELECT * FROM Student WHERE FirstName = @FirstName AND LastName = @LastName)
 	BEGIN
 	PRINT 'We already have a student with the name ' + @FirstName + ' ' + @LastName
 	END
+
 ELSE
 	BEGIN
 	PRINT 'Welcome, ' + @FirstName + ' ' + @LastName
 	END
 
+RETURN
+GO
 
 --4.	Create a procedure called ‘StaffRewards’. It will accept a staff ID as a parameter. If the number of classes the staff member has ever taught is between 0 and 10 print ‘Well done!’, if it is between 11 and 20 print ‘Exceptional effort!’, if the number is greater than 20 print ‘Totally Awesome Dude!’
 
--- create a variable called @StaffID
-DECLARE  @StaffID INT
+DROP PROCEDURE IF EXISTS StaffRewards
+GO
 
--- assign it a literal value
-SET @StaffID = 6
+CREATE PROCEDURE StaffRewards (@StaffID INT = NULL)
+AS
+
+IF @StaffID IS NULL
+	BEGIN
+	RAISERROR('Staff ID is a required param', 16, 1)
+	END
+ELSE
+	BEGIN -- starts my outer ELSE block
+
+	-- create a variable called @NumberOfCourses and assign it a value using data in our DB
+	DECLARE @NumberOfCourses INT
+	SELECT @NumberOfCourses = COUNT(*) FROM Offering WHERE Offering.StaffID = @StaffID
+	-- This would also work if we just had "StaffID" instead of "Offering.StaffID"
+
+	-- if that value is 2 or less: print 'Well done'
+	IF @NumberOfCourses <= 2 -- or: @NumberOfCourses BETWEEN 0 AND 2
+		BEGIN
+		PRINT 'Well done'
+		END
+
+	-- if it's between 3 and 5: print 'Exceptional Effort'
+	ELSE IF @NumberOfCourses BETWEEN 3 AND 5
+		BEGIN
+		PRINT 'Exceptional Effort'
+		END
+
+	-- if it's more than 5: print 'Totally Awesome, Dude.'
+	ELSE --IF @NumberOfCourses > 5
+		BEGIN
+		PRINT 'Totally awesome, dude'
+		END
+	
+	END -- ends my outer ELSE block
+
+
+RETURN
+GO
+
 -- staff ID #1 has taught 0 courses
 -- staff ID 6 has taught 3
 -- staff ID 4 has taught >5
-
--- create a variable called @NumberOfCourses and assign it a value using data in our DB
-DECLARE @NumberOfCourses INT
-SELECT @NumberOfCourses = COUNT(*) FROM Offering WHERE Offering.StaffID = @StaffID
--- This would also work if we just had "StaffID" instead of "Offering.StaffID"
-
--- if that value is 2 or less: print 'Well done'
-IF @NumberOfCourses <= 2 -- or: @NumberOfCourses BETWEEN 0 AND 2
-	BEGIN
-	PRINT 'Well done'
-	END
-
--- if it's between 3 and 5: print 'Exceptional Effort'
-ELSE IF @NumberOfCourses BETWEEN 3 AND 5
-	BEGIN
-	PRINT 'Exceptional Effort'
-	END
-
--- if it's more than 5: print 'Totally Awesome, Dude.'
-ELSE --IF @NumberOfCourses > 5
-	BEGIN
-	PRINT 'Totally awesome, dude'
-	END
-
-
