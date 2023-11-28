a7ea3b9cf5a8c5815d6d84cad1d4c3ebe4fa6b23
Dana <dmarsh@nait.ca>
2023-11-28 11:57:00 -0700

    Nov 28 class examples

--- Fall 2023/Triggers_Sandbox.sql
+++ Fall 2023/Triggers_Sandbox.sql
@@ -45,7 +45,9 @@ GO
 
 USE Movie
 GO
--- Practice #2
+------------------------------ Practice #2 ------------------------------
+-- Create a trigger to enforce a rule that CharacterWages must be >= 0.
+
 
 CREATE TRIGGER TR_PracticeQ2
 	ON MovieCharacter
@@ -75,3 +77,45 @@ UPDATE MovieCharacter SET Characterwage = 500 WHERE CharacterMovie = 'Star Wars'
 UPDATE MovieCharacter SET Characterwage = -5 WHERE CharacterMovie = 'Star Wars'
 
 SELECT * FROM MovieCharacter
+GO
+
+------------------------------ Practice #3 ------------------------------
+-- Create a trigger that enforces a rule that an AgentFee cannot be increased by more than 100% in one update.
+
+CREATE TRIGGER TR_PracticeQ3
+	ON Agent
+	FOR UPDATE
+AS
+
+-- check to see if the trigger needs to run
+IF @@ROWCOUNT > 0 AND UPDATE(AgentFee)
+	BEGIN
+
+	-- check to see if the rule was broken. 
+	-- new value > 2 x old value
+	-- new value: inserted table
+	-- old value: deleted table
+	IF EXISTS (	SELECT * 
+				FROM inserted
+				INNER JOIN deleted ON inserted.AgentID = deleted.AgentID
+				WHERE inserted.AgentFee > 2 * deleted.AgentFee
+	) -- if there are any records which violate the rule
+	-- if so, RAISERROR & ROLLBACK
+		BEGIN
+		RAISERROR('Cannot increase fee by more than 100 percent', 16, 1)
+		ROLLBACK TRANSACTION
+		END
+
+	END
+RETURN
+
+-- test:
+-- does it work for 0 rows?
+UPDATE Agent SET AgentFee = 10000000 WHERE AgentID = 999
+-- does it work for 1 row?
+UPDATE Agent SET AgentFee = 100 WHERE AgentID = 1
+UPDATE Agent SET AgentFee = 1000 WHERE AgentID = 1
+-- does it work for many rows?
+UPDATE Agent SET AgentFee = 200
+
+SELECT * FROM Agent
