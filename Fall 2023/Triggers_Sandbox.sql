81d4981441bce741c6ca96c1d790a693f2b9a870
Dana <dmarsh@nait.ca>
2023-11-28 11:26:28 -0700

    Nov 28 class examples

--- Fall 2023/Triggers_Sandbox.sql
+++ Fall 2023/Triggers_Sandbox.sql
@@ -40,3 +40,38 @@ SET BalanceOwing = 5
 WHERE City = 'Edmonton' -- this should try to update MANY students
 
 SELECT * FROM Student
+
+GO
+
+USE Movie
+GO
+-- Practice #2
+
+CREATE TRIGGER TR_PracticeQ2
+	ON MovieCharacter
+	FOR INSERT, UPDATE 
+AS
+-- first, we make sure there is at least one character whose wage is being updated:
+IF @@ROWCOUNT > 0 AND UPDATE(CharacterWage)
+	BEGIN
+	-- check if the newly inserted or updated records violate the rule
+	-- if so, we roll back
+	IF EXISTS (SELECT * FROM inserted WHERE Characterwage < 0)
+		BEGIN
+		RAISERROR('Cannot have a negative wage.', 16, 1)
+		ROLLBACK TRANSACTION
+		END
+	END
+RETURN
+
+-- test our trigger
+-- does it work as expected for 0 rows?
+UPDATE MovieCharacter SET CharacterWage = -100 WHERE CharacterID = 9999
+-- does it work as expected for 1 row?
+UPDATE MovieCharacter SET CharacterWage = -100 WHERE CharacterID = 3
+UPDATE MovieCharacter SET CharacterWage = 100 WHERE CharacterID = 3
+-- does it work as expected for many rows?
+UPDATE MovieCharacter SET Characterwage = 500 WHERE CharacterMovie = 'Star Wars'
+UPDATE MovieCharacter SET Characterwage = -5 WHERE CharacterMovie = 'Star Wars'
+
+SELECT * FROM MovieCharacter
