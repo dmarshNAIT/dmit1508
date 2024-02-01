fc4d367ab6b60617e18f4e6bb67c0ed97109703a
Dana <dmarsh@nait.ca>
2024-01-31 20:32:38 -0700

    added PKs

--- 1232 - Winter 2024/CreateTable_EmployeeExample.sql
+++ 1232 - Winter 2024/CreateTable_EmployeeExample.sql
@@ -1,4 +1,11 @@
 -- this is a comment
+-- this statement tells SSMS which db to use
+USE Sandbox3
+
+-- if this table exists already, delete it
+DROP TABLE IF EXISTS EmployeeOnProject
+DROP TABLE IF EXISTS Employee
+DROP TABLE IF EXISTS Project
 
 /* hello 
 	this is also a comment
@@ -6,23 +13,28 @@
 
 -- create our Employee table:
 CREATE TABLE Employee (
-	EmployeeID CHAR(11)	NOT NULL
+	EmployeeID CHAR(11)	NOT NULL CONSTRAINT PK_Employee PRIMARY KEY CLUSTERED
 ,	LastName VARCHAR(100) NOT NULL
 ,	FirstName VARCHAR(100) NOT NULL
 )
 
 -- verify my table:
-EXEC sp_help Employee
+-- EXEC sp_help Employee
 
 -- create Project table:
 CREATE TABLE Project (
-	ProjectNumber INT IDENTITY(1,1) NOT NULL
+	ProjectNumber INT IDENTITY(1,1) NOT NULL CONSTRAINT PK_Project PRIMARY KEY CLUSTERED
 ,	ProjectName VARCHAR(50) NOT NULL
 ,	ProjectLocation VARCHAR(50) NOT NULL
 )
+
+-- EXEC sp_help Project
 
 CREATE TABLE EmployeeOnProject (
 	EmployeeID CHAR(11)	NOT NULL
 ,	ProjectNumber INT NOT NULL
 ,	WeeklyHours INT NOT NULL
+,	CONSTRAINT PK_EmployeeOnProject PRIMARY KEY CLUSTERED (EmployeeID, ProjectNumber)
 )
+
+-- EXEC sp_help EmployeeOnProject
