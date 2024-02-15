df39ed4d41d9cd38118b948c6b4e4ccb0ae15a42
Dana <dmarsh@nait.ca>
2024-02-14 20:49:00 -0700

    Feb 14 class examples

--- 1232 - Winter 2024/SELECT_Sandbox.sql
+++ 1232 - Winter 2024/SELECT_Sandbox.sql
@@ -16,6 +16,7 @@ FROM Payment
 -- a dangerous shorcut
 SELECT *
 FROM Student
+-- we will use this as a starting point: NOT in our final queries
 
 -- what if I don't want all the students?
 SELECT FirstName, LastName, City
@@ -30,3 +31,38 @@ WHERE PostalCode LIKE '%Q%' OR PostalCode LIKE '%Z%'
 SELECT FirstName, LastName, PostalCode
 FROM Student
 WHERE PostalCode LIKE '%[QZ]%'
+
+------------ Feb 14 class ------------
+-- aggregate functions
+
+SELECT Mark FROM Registration
+
+-- what is the average mark per student?
+SELECT Avg(Mark) AS 'Average Mark'
+	,	'Average Mark' = Avg(Mark)
+FROM Registration
+
+-- what is the average, total, highest, and lowest payment for a specific student?
+SELECT SUM(Amount) AS 'Total Payments'
+	, AVG(Amount) AS 'Average Payment'
+	, MAX(Amount) AS 'Highest Payment'
+	, MIN(Amount) AS 'Lowest Payment'
+FROM Payment
+WHERE StudentID =  199899200
+
+-- 2 different ways to use COUNT
+SELECT COUNT(*) AS 'Number of Rows in this table'
+, COUNT(DateReleased) AS '# of non-null values in this column'
+FROM Staff
+
+-- what is the average mark per student?
+SELECT StudentID
+	, Avg(Mark) AS 'Average Mark'
+FROM Registration
+GROUP BY StudentID -- " for each student "
+
+-- what is the largest payment made by each student?
+SELECT StudentID
+	, Max(Amount) AS LargestPayment
+FROM Payment
+GROUP BY StudentID
