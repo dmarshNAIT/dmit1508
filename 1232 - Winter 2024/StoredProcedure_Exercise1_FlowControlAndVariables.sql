62639d1c4b0ca226c641389ef7b439937864542a
Dana Marsh <dmarsh@nait.ca>
2024-03-13 21:10:23 -0600

    Mar 13 class examples

--- 1232 - Winter 2024/StoredProcedure_Exercise1_FlowControlAndVariables.sql
+++ 1232 - Winter 2024/StoredProcedure_Exercise1_FlowControlAndVariables.sql
@@ -4,33 +4,47 @@ USE IQSchool
 GO
 
 --Q1: Create a stored procedure called StudentClubCount. It will accept a clubID as a parameter. If the count of students in that club is greater than 2 print ‘A successful club!’ . If the count is not greater than 2 print ‘Needs more members!’.
+DROP PROCEDURE IF EXISTS StudentClubCount
+GO
 
--- create a variable called @ClubID
-DECLARE @ClubID VARCHAR(10)
+CREATE PROCEDURE StudentClubCount ( @ClubID VARCHAR(10) = NULL )
+AS
+	IF @ClubID IS NULL
+		BEGIN
+		PRINT 'Missing parameter'
+		END
+	ELSE -- a parameter WAS provided
+		BEGIN
+		-- create a variable called @StudentCount 
+		DECLARE @StudentCount INT
 
--- give it a starting value
+		-- assign a value to @StudentCount
+		SELECT @StudentCount = COUNT(*)
+		FROM Activity
+		WHERE Activity.ClubId = @ClubID
+
+		-- if @StudentCount is greater than 2, print 'a successful club!'
+		IF @StudentCount > 2
+			BEGIN
+				PRINT 'A successful club!'
+			END
+		-- otherwise, print 'needs more members'
+		ELSE
+			BEGIN
+				PRINT 'Needs more members'
+			END
+	END
+RETURN -- the end of the SP
+
+-- testing this SP:
 -- CHESS club has 1 member, CSS club has 5
-SET @ClubID = 'CSS'
+EXEC StudentClubCount 'CSS'
+EXEC StudentClubCount 'CHESS'
+EXEC StudentClubCount -- testing with no params
+EXEC StudentClubCount 'xyz' -- not a real club
+-- As a bonus challenge, we could update the SP to check if it's a real club.
 
--- create a variable called @StudentCount 
-DECLARE @StudentCount INT
-
--- assign a value to @StudentCount
-SELECT @StudentCount = COUNT(*)
-FROM Activity
-WHERE Activity.ClubId = @ClubID
-
--- if @StudentCount is greater than 2, print 'a successful club!'
-IF @StudentCount > 2
-	BEGIN
-		PRINT 'A successful club!'
-	END
--- otherwise, print 'needs more members'
-ELSE
-	BEGIN
-		PRINT 'Needs more members'
-	END
-
+GO
 
 --Q2: Create a stored procedure called BalanceOrNoBalance. It will accept a studentID as a parameter. Each course has a cost. If the total of the costs for the courses the student is registered in is more than the total of the payments that student has made then print ‘balance owing !’ otherwise print ‘Paid in full! Welcome to School 158!’
 --Do Not use the BalanceOwing field in your solution. 
@@ -70,23 +84,55 @@ ELSE
 --Q3: Create a stored procedure called ‘DoubleOrNothin’. It will accept a students first name and last name as parameters. If the student name already is in the table then print ‘We already have a student with the name firstname lastname!’ Other wise print ‘Welcome firstname lastname!’
 
 -- create a variable for @firstname, and @lastname, then assign them values
+DECLARE @FirstName VARCHAR(25)
+	, @LastName VARCHAR(35)
+
+--SELECT @FirstName = 'Winnie'
+--	, @LastName = 'Woo'
+SELECT @FirstName = 'Dana'
+	, @LastName = 'Marsh'
 
 -- if there EXISTS a student with that name, print 'we already have a student with the name INSERTNAME'
-
+IF EXISTS (SELECT * FROM Student WHERE Student.FirstName = @FirstName AND Student.LastName = @LastName )
+	BEGIN
+		PRINT 'We already have a student with the name ' + @FirstName + ' ' + @LastName
+	END
 -- otherwise, print 'welcome INSERTNAME'
-
+ELSE
+	BEGIN
+		PRINT 'Welcome, ' + @FirstName + ' ' + @LastName
+	END
 -- Q4: Create a procedure called ‘StaffRewards’. It will accept a staff ID as a parameter. If the number of classes the staff member has ever taught is between 0 and 10 print ‘Well done!’, if it is between 11 and 20 print ‘Exceptional effort!’, if the number is greater than 20 print ‘Totally Awesome Dude!’
 
 -- create a variable for @StaffID, give it a value
+DECLARE @StaffID SMALLINT
+SET @StaffID = 4 
+-- staff ID 1 has taught 0 courses
+-- staff ID 6 has taught 3
+-- staff ID 4 has taught >5
 
 -- create a variable for @NumberOfCourses, give it a value
+DECLARE @NumberOfCourses SMALLINT
+
+SELECT @NumberOfCourses = COUNT(CourseID) -- or COUNT(*)
+FROM Offering
+WHERE Offering.StaffID = @StaffID
 
 -- if value is 2 or less, print 'well done!'
-
+IF @NumberOfCourses <= 2
+	BEGIN
+		PRINT 'Well done!'
+	END
 -- if it's 3-5, print 'exceptional effort!'
-
+ELSE IF @NumberOfCourses BETWEEN 3 AND 5
+	BEGIN
+		PRINT 'Exceptional Effort!'
+	END
 -- if it's >5, print 'totally awesome, dude'
-
+ELSE
+	BEGIN
+		PRINT 'Totally awesome, dude.'
+	END
 
 
 
