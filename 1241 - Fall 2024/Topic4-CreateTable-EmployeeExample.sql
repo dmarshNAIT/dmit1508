357e424cdb6f5ab9c4ad1f791c3867156f0a845b
Dana <dmarsh@nait.ca>
2024-09-26 14:28:18 -0600

    Add first draft

---
+++ 1241 - Fall 2024/Topic4-CreateTable-EmployeeExample.sql
@@ -0,0 +1,14 @@
+CREATE TABLE Employee (
+....
+
+
+CREATE TABLE EmployeeOnProject (
+....
+
+
+CREATE TABLE Project (
+.....
+
+EXEC sp_help Employee
+EXEC sp_help EmployeeOnProject
+EXEC sp_help Project
