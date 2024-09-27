6f5061212735297cf9d606520724aa23e9d1f069
Dana Marsh <dmarsh@nait.ca>
2024-09-27 16:54:42 -0600

    Add PK to Employee Project example 🔑

--- 1241 - Fall 2024/Topic4-CreateTable-EmployeeExample.sql
+++ 1241 - Fall 2024/Topic4-CreateTable-EmployeeExample.sql
@@ -1,13 +1,24 @@
+DROP TABLE IF EXISTS Employee
+
 CREATE TABLE Employee (
-....
-
-
-CREATE TABLE EmployeeOnProject (
-....
-
+	EmployeeID CHAR(11)	NOT NULL CONSTRAINT PK_Employee PRIMARY KEY CLUSTERED,
+	LastName VARCHAR(50) NOT NULL,
+	FirstName VARCHAR(50) NOT NULL
+)
 
 CREATE TABLE Project (
-.....
+	ProjectNumber INT IDENTITY(1,1) NOT NULL 
+		CONSTRAINT PK_Project PRIMARY KEY CLUSTERED,
+	ProjectName VARCHAR(100) NOT NULL,
+	ProjectLocation VARCHAR(100) NOT NULL
+)
+
+CREATE TABLE EmployeeOnProject (
+	EmployeeID CHAR(11) NOT NULL,
+	ProjectNumber INT NOT NULL,
+	WeeklyHours INT NOT NULL,
+	CONSTRAINT PK_EmployeeOnProject PRIMARY KEY CLUSTERED (EmployeeID, ProjectNumber)
+)
 
 EXEC sp_help Employee
 EXEC sp_help EmployeeOnProject
