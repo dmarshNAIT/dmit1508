e65d95dde7b5e50d1dda1fd9a7c81b7f4140c406
Dana <dmarsh@nait.ca>
2024-10-10 15:53:55 -0600

    Add SELECT examples 🩴

--- 1241 - Fall 2024/Topic6-Select-Sandbox.sql
+++ 1241 - Fall 2024/Topic6-Select-Sandbox.sql
@@ -19,5 +19,26 @@ SELECT Amount
 FROM Payment
 
 -- what if I want all the columns in the Student table?
-SELECT * FROM Student
+SELECT * 
+FROM Student
 -- very dangerous, very risky
+
+-- what if we want to filter records?
+SELECT FirstName, LastName, City 
+FROM Student
+WHERE City = 'Edmonton'
+
+-- I want to see all the students whose lastname starts with K
+SELECT FirstName, LastName, City 
+FROM Student
+WHERE LastName LIKE 'K%'
+
+-- I want to see students in Edmonton OR Calgary
+SELECT FirstName, LastName, City 
+FROM Student
+WHERE City = 'Edmonton' OR City = 'Calgary'
+
+-- or:
+SELECT FirstName, LastName, City 
+FROM Student
+WHERE City IN ('Edmonton', 'Calgary')
