c43b4cc27a4b6159e013793f315addbf3cc1536a
Dana Marsh <dmarsh@nait.ca>
2024-10-11 19:49:53 -0600

    Add aggregation examples 🧮

--- 1241 - Fall 2024/Topic6-Select-Sandbox.sql
+++ 1241 - Fall 2024/Topic6-Select-Sandbox.sql
@@ -1,4 +1,4 @@
--- October 10: Intro to SELECT
+---------------------------------- October 10: Intro to SELECT ----------------------------------
 
 -- tell SSMS which db to use
 USE IQSchool
@@ -42,3 +42,54 @@ WHERE City = 'Edmonton' OR City = 'Calgary'
 SELECT FirstName, LastName, City 
 FROM Student
 WHERE City IN ('Edmonton', 'Calgary')
+
+---------------------------------- October 11: Aggregations ----------------------------------
+
+-- look at all the payments in the payment table
+SELECT Amount
+FROM Payment
+
+-- what is the overall average amount across all payments?
+SELECT  AVG(Amount) AS AveragePayment
+FROM Payment
+
+-- tell me the largest & smallest payments
+SELECT MIN(Amount) AS LowestPayment
+, MAX(Amount) AS HighestPayment
+, SUM(Amount) AS TotalPayments
+, COUNT(*) AS NumberOfRows
+FROM Payment
+
+-- what are the 2 ways to use COUNT?
+SELECT COUNT(*) AS NumberOfStaff
+	, COUNT(StaffID) AS NumberOfRecordsWithAStaffID
+	, COUNT(DateReleased) AS NumberOfRecordsWithADateReleased
+FROM Staff
+
+-- CHALLENGE:
+-- how many students live in Calgary
+SELECT COUNT(*) AS NumberOfStudents
+FROM Student
+WHERE City = 'Calgary'
+
+-- what is the highest balance owing?
+-- what is the total balance owing?
+SELECT MAX(BalanceOwing) AS HighestBalance
+	, SUM(BalanceOwing) AS TotalBalanceOwing
+FROM Student
+
+-- what if I want to aggregate across categories?
+-- what is the average payment per payment type?
+SELECT PaymentTypeID
+	, AVG(Amount) AS AverageAmount
+FROM Payment
+GROUP BY PaymentTypeID -- "for each PaymentTypeID"
+
+-- I want to see the students with an average mark > 75
+SELECT StudentID
+	, Avg(Mark) AS AverageMark
+FROM Registration
+GROUP BY StudentID
+HAVING Avg(Mark) > 75
+
+
