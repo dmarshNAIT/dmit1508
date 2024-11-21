d45dd75a6f30be54f7cce2df0709d3a572d707a9
Dana <dmarsh@nait.ca>
2024-11-21 13:45:44 -0700

    Add DML example 🍞

---
+++ 1241 - Fall 2024/Topic8-DMLExercise.sql
@@ -0,0 +1,64 @@
+--Stored Procedures – DML exercise
+--Use the IQSchool database
+USE IQSchool
+GO
+
+--1. Create a stored procedure called ‘AddClub’ to add a new club record.
+
+-- create a procedure called AddClub
+-- it will need parameters
+-- I need to check if params were provided. If not, raise an error.
+-- if I have all the params, then INSERT a new Club record
+
+CREATE PROCEDURE AddClub (@ClubID VARCHAR(10) = NULL, @ClubName VARCHAR(50) = NULL)
+AS
+
+IF @ClubID IS NULL OR @ClubName IS NULL
+	BEGIN --1
+	RAISERROR('ClubID and ClubName are both required fields.', 16, 1)
+	END  --1
+ELSE -- we DO have parameters
+	BEGIN  --2
+	INSERT INTO Club (ClubID, ClubName) VALUES (@ClubID, @ClubName)
+		IF @@ERROR != 0 --something went wrong
+			BEGIN --3
+			RAISERROR('Error when adding club', 16, 1)
+			END --3
+	END  --2
+RETURN -- end of the SP
+GO -- end of the batch
+
+-- test:
+-- test with missing params:
+EXEC AddClub
+
+-- test with valid params:
+EXEC AddClub 'RBAS', 'Rodney Bread Appreciation Society'
+SELECT * FROM Club
+
+-- test with params that will cause the INSERT to fail:
+EXEC AddClub 'RBAS', 'Really Bad Archiecture Society'
+
+
+
+--2. Create a stored procedure called ‘DeleteClub’ to delete a club record.
+--3. Create a stored procedure called ‘Updateclub’ to update a club record. Do not update the
+--primary key!
+--4. Create a stored procedure called ‘ClubMaintenance’. It will accept parameters for both ClubID
+--and ClubName as well as a parameter to indicate if it is an insert, update or delete. This
+--parameter will be ‘I’, ‘U’ or ‘D’. Insert, update, or delete a record accordingly. Focus on making
+--your code as efficient and maintainable as possible.
+--5. Create a stored procedure called ‘RegisterStudent’ that accepts StudentID and OfferingCode as
+--parameters. If the number of students in that Offering is not greater than the Max Students for
+--that course, add a record to the Registration table and add the cost of the course to the
+--student’s balance. If the registration would cause the Offering to have greater than the
+--MaxStudents raise an error.
+--6. Create a procedure called ‘StudentPayment’ that accepts PaymentID, Student ID,
+--paymentamount, and paymentTypeID as parameters. Add the payment to the payment table
+--and adjust the students balance owing to reflect the payment.
+--7. Create a stored procedure called ‘FireStaff’ that will accept a StaffID as a parameter. Fire the
+--staff member by updating the record for that staff and entering today’s date as the
+--DateReleased.
+--8. Create a stored procedure called ‘WithdrawStudent’ that accepts a StudentID, and OfferingCode
+--as parameters. Withdraw the student by updating their Withdrawn value to ‘Y’ and subtract ½
+--of the cost of the course from their balance. If the result would be a negative balance set it to 0.
